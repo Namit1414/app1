263ac82cca9a9ada2741034d5eaf115b6534571e
Namit1414 <66672652+Namit1414@users.noreply.github.com>
2020-06-14 02:51:23 +0530

    Add files via upload

---
+++ lists2.py
@@ -0,0 +1,38 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.list import OneLineListItem, MDList, TwoLineListItem, ThreeLineIconListItem
+from kivymd.uix.list import OneLineIconListItem, IconLeftWidget
+from kivy.uix.scrollview import ScrollView
+
+
+class DemoApp(MDApp):
+
+    def build(self):
+        screen = Screen()
+
+        # Creating a Simple List
+        scroll = ScrollView()
+
+        list_view = MDList()
+        for i in range(20):
+
+            item = ThreeLineIconListItem(text=str(i) + ' item',
+                                      secondary_text='This is ' + str(i) + 'th item',
+                                      tertiary_text='hello')
+
+            icons = IconLeftWidget(icon="cake-layered")
+            #items = OneLineIconListItem(text=str(i) + ' item')
+
+
+            item.add_widget(icons)
+            list_view.add_widget(item)
+           # list_view.add_widget(items)
+
+        scroll.add_widget(list_view)
+        # End List
+
+        screen.add_widget(scroll)
+        return screen
+
+
+DemoApp().run()
